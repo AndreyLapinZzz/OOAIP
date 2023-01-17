004e214d31b6cf61cd2bcd0ef00ff37c428419c4
Andrey <andylapin100@mail.ru>
2023-01-17 13:08:48 +0300

    some fixes

--- SpaceBattle.Lib/StartCommand/StartCommand.cs
+++ SpaceBattle.Lib/StartCommand/StartCommand.cs
@@ -12,9 +12,9 @@ public class StartCommand : ICommand
 
     public void execute()
     {
-        obj.properties.ToList().ForEach(o => IoC.Resolve<ICommand>("SpaceBattle.SetProperty", obj.uobject, o.Key, o.Value).execute());
-        var moveCmd = IoC.Resolve<ICommand>("SpaceBattle.Commands.Move", obj.uobject);
-        IoC.Resolve<ICommand>("SpaceBattle.SetProperty", obj.uobject, "Move", moveCmd).execute();
-        IoC.Resolve<ICommand>("SpaceBattle.QueuePush", moveCmd).execute();
+        obj.properties.ToList().ForEach(o => IoC.Resolve<ICommand>("Game.SetProperty", obj.uobject, o.Key, o.Value).execute());
+        var moveCmd = IoC.Resolve<ICommand>("Game.Commands.Move", obj.uobject);
+        IoC.Resolve<ICommand>("Game.SetProperty", obj.uobject, "Move", moveCmd).execute();
+        IoC.Resolve<ICommand>("Game.QueuePush", moveCmd).execute();
     }
 }
