d93bdc907b164bfd1398ce49c7ea1117cffa4a52
Alina Klyuchnikova <kalinas2508@gmail.com>
2024-12-16 23:06:11 +0600

    Bugs fix

--- SpaceBattle.Lib/Server/StopServerCommand.cs
+++ SpaceBattle.Lib/Server/StopServerCommand.cs
@@ -4,15 +4,16 @@ using System.Collections.Concurrent;
 
 public class StopServerCommand : ICommand
 {
-    public void Execute() {
-        IoC.Resolve<ConcurentList<int>>("ThreadsIDs").foreach(id => {
+    public void Execute(){
+        int[] IdsArray = IoC.Resolve<BlockingCollection<int>>("ThreadsIDs").ToArray();
+        foreach (int id in IdsArray) {
             ICommand softStopCommand = IoC.Resolve<ICommand>("Soft Stop The Thread", id);
-            try() {
-            softStopCommand.execute();
-            } catch(Exception e) {
-            IoC.Resolve<ICommand>("ExceptionHandler", e, current).execute();
+            try(){
+                softStopCommand.execute();
+            } catch (Exception e)
+            {
+                IoC.Resolve<ICommand>("ExceptionHandler", e, current).execute();
             }
         }
-        )
     }
 }
