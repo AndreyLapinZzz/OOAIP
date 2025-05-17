76a88471778f8ba42a366d3832e0eca06195e85e
KlyuchnikovaAlinaFIT-211 <115130492+KlyuchnikovaAlinaFIT-211@users.noreply.github.com>
2025-05-17 17:46:14 +0600

    Revert "lab6_2 - Console server"

--- SpaceBattle.Lib/Strategies/StartServerStrategy.cs
+++
@@ -1,9 +0,0 @@
-namespace SpaceBattle.Lib;
-using Hwdtech;
-
-public class StartServerStrategy : IStrategy
-{
-    public object RunStrategy(params object[] args){
-        return new StartServerCommand((uint)args[0]);
-    }
-}
